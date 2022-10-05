835c92b573d14661c5ddad0e8d01b90e4a951efc
John McAllister <43422088+jmcall10@users.noreply.github.com>
2022-10-04 21:04:53 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cyphlife
+
+#option to load verbage from a saved list
+
+#transcribe voice input
+#will we need ai to clean it up?
+#how will that execute in real time?
--- rhyme.py
+++ rhyme.py
@@ -6,18 +6,24 @@ import time
 import requests
 
 
-topic = input("Enter topic: ")
-print("Topic is: " + topic)
-api_url = 'https://api.datamuse.com/words?rel_rhy=' + topic + '&max=10'
+
 
 def use_requests(api_url):
 
     response = requests.get(api_url)
     json_response = json.loads(response.text)
 
-
     for data in (json_response):
-        print(data['word'])
+        print("" + (data['word'].upper()) + " / score: " + str(data['score']))
+        api_url = 'https://api.datamuse.com/words?ml=' + data['word'] + '&max=4'
+        response = requests.get(api_url)
+        json_response = json.loads(response.text)
+        for data in json_response:
+            print("     related word: " + data['word'].upper())
+
+
+        #time.sleep(1)
+        #print(json_response)
     #for count, data in enumerate(json_response):
     #    while count < 10:
     #        print(count, data['word'] + '....' )
@@ -29,10 +35,20 @@ def use_requests(api_url):
     #print(d = json_response[4])
     #webbrowser.open_new_tab(photo_url)
 
-    return
+    return json_response
+
+topic = input("Enter topic: ")
+print("Topic is: " + topic)
+api_url = 'https://api.datamuse.com/words?rel_rhy=' + topic + '&max=10'
 
 use_requests(api_url)
-time.sleep(5)
-api_url = 'https://api.datamuse.com/words?ml=' + topic
+time.sleep(1)
+#api_url = 'https://api.datamuse.com/words?ml=' + topic + '&max=10'
 #use_requests(api_url)
-use_requests(api_url)
+#use_requests(api_url)
+
+#poem from a picture
+
+#shuffle prompts? cyph life
+
+#poem app where daily u have a prompt to make a poem
